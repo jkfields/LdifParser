d96164b33703e3e32bada9ecc1dce3033cad1516
Jeff Fields <jkfields@yahoo.com>
2021-02-01 10:27:24 -0500

    Update ldif.py

--- ldif.py
+++ ldif.py
@@ -5,48 +5,55 @@
 
 class LdifParsingException(Exception):
     pass
-  
+
 class LdifParser:
-  def parse(self, ldifattrs):
-      """
-      parse the ldif attribute data received from an ldapsearch
-      query against a Sun/Oracle Directory Server
+    def parse(self, ldifattrs)
+        """
+        parse the ldif attribute data received from an ldapsearch
+        query against a Sun/Oracle Directory Server
       
-      each record can have multiple values for a single attribute
-      each record is separated by a blank line
-      """
-      try:
-        # process each line of the block of data
-        for ln in ldifattrs.split('\n'):
-          if not rec in locals():  rec = {}
-          
-          # is this the blank line?
-          if ln.strip():
-              # the attr and value are colon-separated; some values are also comma separate; split
-              # on the 1st colon in the string
-              attr, value = [ str.strip() for str in ln.split(':', 1) ]
-            
-              # convert and number values to int, boolean values, and date string to ISO-8601 format
-              if value.isdigit():  value = int(value)
-              elif value.lower() == 'true':  value = True
-              elif value.lower() == 'false':  value = False
-              #elif "Time" in attr:  value = dtToIso(value)
-              else:  pass
-          
-              # check for additional values for existing attr
-              if attr in rec.keys():
-                  # if first duplicate, converts dict value to list
-                  if not isinstance(rec.get(attr), list):
-                      rec[attr] = [ lrec.get(attr) ]
-                      
-                  # append the new value
-                  rec[attr].append(value)
-              else:
-                  # add the attr and value to the dict
-                  rec[attr] = value
-          else:
-              yield rec
-              rec.clear()
-              
-      except Exception as err:
-        raise LdifParsingException(err)
+        :: each record can have multiple values for a single attribute
+        :: each record is separated by a blank line
+        :: attributes are colon-separated; some values are also
+           colon separated; split line on 1st colon
+        """
+        try:
+            # process each line of the block of data
+            for ln in ldifattrs.split('\n'):
+                if not rec in locals():  rec = {}
+                
+                # is this the blank line?
+                if ln.strip():
+                    # attr and value are colon-separated; some values are also comma separate; 
+                    # split on the 1st colon in the string
+                    attr, value = [ str.strip() for str in ln.split(':', 1) ]
+                    
+                    # convert and number values to int, boolean values, and date string to ISO-8601 format
+                    if value.isdigit():  value = int(value)
+                    elif value.lower() == 'true':  value = True
+                    elif value.lower() == 'false':  value = False
+                    elif "Time" in attr:  value = dtToIso(value)
+                    else:  pass
+                    
+                    # check for additional values for existing attrs
+                    if attr in rec.keys():
+                        # if first duplicate, converts dict value to list
+                        if not isinstance(rec.get(attr), list):
+                            rec[attr] = [ lrec.get(attr) ]
+                            
+                            # append the new value
+                            rec[attr].append(value)
+                    else:
+                        # add the attr and value to the dict
+                        rec[attr] = value
+                else:
+                    yield rec
+                    rec.clear()
+                    
+        except Exception as err:
+            raise LdifParsingException(err)
+    
+    @static
+    def dtToIso(dtstr):
+        pass
+        
